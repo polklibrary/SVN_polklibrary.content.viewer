3433da8da6b1836f84d013acdc2452c310a62ced
hietpasd <hietpasd@uwosh.edu>
2017-08-18 08:10:02 -0500

    fixed fod embeds

--- polklibrary.content.viewer/src/polklibrary/content/viewer/utility.py
+++ polklibrary.content.viewer/src/polklibrary/content/viewer/utility.py
@@ -10,7 +10,7 @@ ALMA_NAME = 'Catalog'
 ALEXANDER_STREET_TARGET = 'asp'
 KANOPY_TARGET = 'kan'
 FILMSONDEMAND_TARGET = 'fod'
-ALMA_TARGET = 'alma'
+ALMA_TARGET = 'uwi'
 
 def ResourceEnhancer(id, title):
 
@@ -34,14 +34,15 @@ def ResourceEnhancer(id, title):
         
     elif FILMSONDEMAND_TARGET in id.lower():
         data['name'] = FILMSONDEMAND_NAME
-        data['base_url'] = 'https://www.remote.uwosh.edu/login?url=http://fod.infobase.com/PortalPlaylists.aspx?wID=102638&xtid=' + data['id']
-        data['embed'] = '<iframe frameborder="0" scrolling="no" width="700" height="450" src="https://www.remote.uwosh.edu/login?url=https://fod.infobase.com/OnDemandEmbed.aspx?token=' + data['id'] + '&wID=102638&plt=FOD&loid=0&w=480&h=340&fWidth=700&fHeight=450" allowfullscreen >&nbsp;</iframe>'
+        data['base_url'] = 'https://www.remote.uwosh.edu/login?url=http://fod.infobase.com/PortalPlaylists.aspx?wID=240117&xtid=' + data['id']
+        data['embed'] = '<iframe frameborder="0" scrolling="no" width="700" height="450" src="https://www.remote.uwosh.edu/login?url=https://fod.infobase.com/OnDemandEmbed.aspx?token=' + data['id'] + '&wID=240117&plt=FOD&loid=0&w=700&h=420&fWidth=700&fHeight=420" allowfullscreen >&nbsp;</iframe>'
             
     elif ALMA_TARGET in id.lower():
         data['name'] = ALMA_NAME
-        data['base_url'] = 'https://www.remote.uwosh.edu/login?url=http://fod.infobase.com/PortalPlaylists.aspx?wID=102638&xtid=' + data['id']
+        data['base_url'] = 'https://uw-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=' + data['id'] + '&context=L&vid=OSH&search_scope=OSH_ALL&tab=default_tab&lang=en_US'
         data['embed'] = ''
-    
+        #https://fod.infobase.com/OnDemandEmbed.aspx?token=38153&wID=240117&plt=FOD&loid=0&w=640&h=480&fWidth=660&fHeight=530
+        
     return data
     
         
@@ -51,8 +52,10 @@ def CleanID(id):
     elif KANOPY_TARGET in id.lower():
         id = id.split('-').pop()
         return re.sub("\D", "", id)
-    else:
-        return id.lstrip('1').lstrip('0')
+    elif FILMSONDEMAND_TARGET in id.lower():
+        return re.sub("\D", "", id).lstrip('1').lstrip('0')
+    elif ALMA_TARGET in id.lower():
+        return id
         
         
     
